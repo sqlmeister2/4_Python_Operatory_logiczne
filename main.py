7c8217ed511f992ae57c99331485b80d465befd5
MarioPC <sqlmeister2@gmail.com>
2024-10-23 22:10:17 +0200

    zakomentowanie kodów w celu uruchamiania kodów częsciami

--- main.py
+++ main.py
@@ -2,25 +2,33 @@ wiek = 12
 kasa = 40
 
 #zagnieżdzanie ifów
-if wiek >= 18:
-    if kasa >= 35:
-        print("Możesz wejść")
+# if wiek >= 18:
+#     if kasa >= 35:
+#         print("Możesz wejść")
 
-#operatory logiczne:
-if wiek >= 18 and kasa >= 35:
-    print("Możesz wejść!")
+# # #operatory logiczne:
+# if wiek >= 18 and kasa >= 35:
+#     print("Możesz wejść!")
+# else:
+#     print("Niestety nie możesz wejść")
 
-if wiek <= 12 or kasa >= 35:
-    print("Możecie wejść na film dla dzieci")
+# if wiek <= 12 or kasa >= 35:
+#     print("Możecie wejść na film dla dzieci")
 
-#negacja
-if not wiek > 12:
-    print("Możecie wleźć")
+# #negacja
+# if not wiek > 12:
+#     print("Nie możecie wleźć")
 
-#kolejność łączenia warunkow
-if True or False and False: #najpierw się wykonało and a potem or
-    print("Prawda")
+# #kolejność łączenia warunkow
+# if True or False and False: #najpierw się wykonało and a potem or
+#     print("Prawda")
+# else:
+#     print("Fałsz")
+
+##najbezpieczniej obejmować warunki nawiasami
+if True and (False or False):
+    print('Prawda')
 else:
-    print("Fałsz")
+    print('nieprawda')
 
 
